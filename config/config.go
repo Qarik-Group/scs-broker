c431b96f0e9317995b6d05647d3792bebecf602e
Justin Carter <justin@misthos.io>
2020-06-10 14:54:10 +0200

    Some cleanup

--- config/config.go
+++ config/config.go
@@ -9,24 +9,18 @@ import (
 const ConfigEnvVarName string = "CONFIG_SERVER_BROKER_CONFIG"
 
 type Config struct {
-	ServiceName                 string `yaml:"service_name"`
-	ServiceID                   string `yaml:"service_id"`
-	SharedVMPlanID              string `yaml:"shared_vm_plan_id"`
-	Host                        string `yaml:"host"`
-	DefaultConfigPath           string `yaml:"redis_conf_path"`
-	ProcessCheckIntervalSeconds int    `yaml:"process_check_interval"`
-	StartRedisTimeoutSeconds    int    `yaml:"start_redis_timeout"`
-	InstanceDataDirectory       string `yaml:"data_directory"`
-	PidfileDirectory            string `yaml:"pidfile_directory"`
-	InstanceLogDirectory        string `yaml:"log_directory"`
-	ServiceInstanceLimit        int    `yaml:"service_instance_limit"`
-	Description                 string `yaml:"description"`
-	LongDescription             string `yaml:"long_description"`
-	ProviderDisplayName         string `yaml:"provider_display_name"`
-	DocumentationURL            string `yaml:"documentation_url"`
-	SupportURL                  string `yaml:"support_url"`
-	DisplayName                 string `yaml:"display_name"`
-	IconImage                   string `yaml:"icon_image"`
+	ServiceName          string `yaml:"service_name"`
+	ServiceID            string `yaml:"service_id"`
+	SharedVMPlanID       string `yaml:"shared_vm_plan_id"`
+	Host                 string `yaml:"host"`
+	ServiceInstanceLimit int    `yaml:"service_instance_limit"`
+	Description          string `yaml:"description"`
+	LongDescription      string `yaml:"long_description"`
+	ProviderDisplayName  string `yaml:"provider_display_name"`
+	DocumentationURL     string `yaml:"documentation_url"`
+	SupportURL           string `yaml:"support_url"`
+	DisplayName          string `yaml:"display_name"`
+	IconImage            string `yaml:"icon_image"`
 }
 
 func ParseConfig() (Config, error) {
