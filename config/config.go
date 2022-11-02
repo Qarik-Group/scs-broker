501ae1bef7810694ef247c7a6685db00846d0f03
Chad Kullhem <digitaleagle@gmail.com>
2022-11-02 17:02:33 -0600

    Fixed issues around service plan ids and services

--- config/config.go
+++ config/config.go
@@ -9,29 +9,35 @@ import (
 const ConfigEnvVarName string = "SCS_BROKER_CONFIG"
 
 type Config struct {
-	ReleaseTag                string   `yaml:"config_server_release_tag"`
-	Auth                      Auth     `yaml:"broker_auth"`
-	ServiceName               string   `yaml:"service_name"`
-	ServiceID                 string   `yaml:"service_id"`
-	BasicPlanId               string   `yaml:"basic_plan_id"`
-	BasicPlanName             string   `yaml:"basic_plan_name"`
-	CfConfig                  CfConfig `yaml:"cloud_foundry_config"`
-	Description               string   `yaml:"description"`
-	LongDescription           string   `yaml:"long_description"`
-	ProviderDisplayName       string   `yaml:"provider_display_name"`
-	DocumentationURL          string   `yaml:"documentation_url"`
-	SupportURL                string   `yaml:"support_url"`
-	DisplayName               string   `yaml:"display_name"`
-	IconImage                 string   `yaml:"icon_image"`
-	InstanceSpaceGUID         string   `yaml:"instance_space_guid"`
-	InstanceDomain            string   `yaml:"instance_domain"`
-	ConfigServerDownloadURI   string   `yaml:"config_server_download_uri"`
-	RegistryServerDownloadURI string   `yaml:"registry_server_download_uri"`
+	Auth                Auth      `yaml:"broker_auth"`
+	BrokerName          string    `yaml:"broker_name"`
+	BrokerID            string    `yaml:"broker_id"`
+	ArtifactsDir        string    `yaml:"artifacts_directory"`
+	CfConfig            CfConfig  `yaml:"cloud_foundry_config"`
+	Description         string    `yaml:"description"`
+	LongDescription     string    `yaml:"long_description"`
+	ProviderDisplayName string    `yaml:"provider_display_name"`
+	DocumentationURL    string    `yaml:"documentation_url"`
+	SupportURL          string    `yaml:"support_url"`
+	DisplayName         string    `yaml:"display_name"`
+	IconImage           string    `yaml:"icon_image"`
+	InstanceSpaceGUID   string    `yaml:"instance_space_guid"`
+	InstanceDomain      string    `yaml:"instance_domain"`
+	Services            []Service `yaml:"services"`
 }
 
 type Auth struct {
 	Username string `yaml:"user"`
 	Password string `yaml:"password"`
+}
+
+type Service struct {
+	ServiceName        string `yaml:"service_name"`
+	ServiceID          string `yaml:"service_id"`
+	ServicePlanID      string `yaml:"service_plan_id"`
+	ServicePlanName    string `yaml:"service_plan_name"`
+	ServiceDescription string `yaml:"service_description"`
+	ServiceDownloadURI string `yaml:"service_download_uri"`
 }
 
 type CfConfig struct {
